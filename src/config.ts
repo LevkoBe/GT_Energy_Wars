33b0fac1a3beba725d0e994d667802328c8bcf11
LevkoBe <lbeniakh@kse.org.ua>
2025-06-29 18:30:23 +0300

    feat: config

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const NUM_PLAYERS: number = 3;
+export const NUM_NODES: number = 25;
+
+export const PADDING: number = 100;
+export const MIN_NODE_DISTANCE: number = 60;
+
+export const BASE_NODE_SIZE: number = 24;
+export const NODE_SIZE_VARIANCE: number = 6;
+export const SOCIAL_CONNECTION_CHANCE: number = 0.3;
+
+export const INITIAL_PLAYER_ENERGY: number = 50;
+export const ENERGY_TOWER_OUTPUT: number = 50;
+export const MIN_POWERLINE_CAPACITY: number = 9;
+export const MAX_POWERLINE_CAPACITY: number = 12;
+export const POWERLINE_COST_DISTANCE_DIVISOR: number = 10;
+
+export const ENERGY_DEFICIT_THRESHOLD: number = 3;
+export const SABOTAGE_VOTE_THRESHOLD: number = 3;
+export const CLUSTER_BALANCE_TOLERANCE: number = 0.3;
